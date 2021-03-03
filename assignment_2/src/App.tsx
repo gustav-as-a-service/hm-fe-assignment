e7da3db7f3fce704378cf2547544bb4286e71d46
Gustav <gqstav@gmail.com>
2021-03-03 19:52:32 +0100

    feat: add SPA canvas

--- assignment_2/src/App.tsx
+++ assignment_2/src/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const App = () => {
-	return <div/>;
+	return <div style={{
+		display: "flex",
+		height: "100vh",
+		width: "100vw",
+		justifyContent: "center",
+		backgroundColor: "lightgrey",
+	}}>
+		<div style={{
+			width: "800px",
+		}}>
+		</div>
+	</div>;
 };
