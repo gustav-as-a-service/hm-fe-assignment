5fabe0e92ee549124ced171075f882a192a004b2
Gustav <gqstav@gmail.com>
2021-03-05 18:28:20 +0100

    feat: create hook for reaching the api

---
+++ assignment_2/src/Components/SearchForm/organisms/Suggestions/hooks/useStarWarsSearch.tsx
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import {useSearch as useSearchImport} from "../../SearchFormProvider/SearchProvider";
+import {api as apiImport} from "../../../../../api/api";
+
+type StarWarsCharacter = {
+  "name": string;
+  "height": string;
+  "mass": string;
+  "hair_color": string;
+  "skin_color": string;
+  "eye_color": string;
+  "birth_year": string;
+  "gender": string;
+  "homeworld": string;
+}
+
+type UseStarWarsSearchProps = {
+  useSearch?: typeof useSearchImport;
+  api?: typeof apiImport
+}
+
+export const useStarWarsSearch = ({useSearch = useSearchImport, api = apiImport}: UseStarWarsSearchProps = {}) => {
+	const {search} = useSearch();
+	const [searchResults, setSearchResults] = useState<StarWarsCharacter[] | undefined>(
+		undefined);
+	const [responseStatus, setResponseStatus] = useState<number | undefined>(
+		undefined);
+
+	useEffect(() => {
+		if (search.length > 0) {
+			api.getPeople(search).then(response => {
+				if (response.status !== 200) {
+					throw response.status;
+				}
+
+				return response.json();
+			}).then(({results}) => {
+				setSearchResults(results);
+			}).catch((error) => {
+				setResponseStatus(error);
+			});
+		}
+
+		if (search.length === 0) {
+			setSearchResults(undefined);
+		}
+	}, [search]);
+
+	return {searchResults, responseStatus};
+};
