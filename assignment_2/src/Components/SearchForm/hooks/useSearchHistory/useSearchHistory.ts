737fea2d34be1e0652115d1f0417ea2db23dcc8b
Gustav <gqstav@gmail.com>
2021-03-06 22:32:17 +0100

    feat(history-hook): add C & D operations

--- assignment_2/src/Components/SearchForm/hooks/useSearchHistory/useSearchHistory.ts
+++ assignment_2/src/Components/SearchForm/hooks/useSearchHistory/useSearchHistory.ts
@@ -8,6 +8,20 @@ type SearchHistoryItem = {
 export const useSearchHistory = () => {
 	const [searchHistory, setSearchHistory] = useState<SearchHistoryItem[]>([]);
 
+	const appendSearchItem = useCallback((search: string) => {
+		const newSearchHistory = [...searchHistory, {search, date: Date.now()}];
+		localStorage.setItem("starWarsSearch", JSON.stringify(newSearchHistory));
+
+	}, [searchHistory]);
+
+	const deleteSearchItem = useCallback((indexToDelete: number) => {
+		const newSearchHistory = searchHistory.filter(
+			(value, index) => index !== indexToDelete);
+		setSearchHistory(newSearchHistory);
+		localStorage.setItem("starWarsSearch", JSON.stringify(newSearchHistory));
+
+	}, [searchHistory]);
+
 	// We only care about showing history when user re-mounts the component, i.e. refresh page, hence
 	// we use this hook
 	useEffect(() => {
@@ -19,6 +33,6 @@ export const useSearchHistory = () => {
 		}
 	}, []);
 
-	return {searchHistory};
+	return {searchHistory, appendSearchItem, deleteSearchItem};
 };
 
